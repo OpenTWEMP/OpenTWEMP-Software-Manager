15a20a9ba4afcee3e957c24b1882ca629ec90634
Denis Rezepov <me@dar920910.ru>
2024-03-19 12:49:07 +0300

    Add project files.

---
+++ TWEMP.SoftwareManager.ConsoleClient/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TWEMP.SoftwareManager.ConsoleClient;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Console Client for OpenTWEMP Software Manager!");
+    }
+}
