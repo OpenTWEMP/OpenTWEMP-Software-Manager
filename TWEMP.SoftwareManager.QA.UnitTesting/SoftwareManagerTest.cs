ba5103189690156eb7fea16ac8f254628e3c69be
Denis Rezepov <me@dar920910.ru>
2024-03-20 09:01:23 +0300

    Added the TWEMP.SoftwareManager.QA.UnitTesting project template to the solution.

---
+++ TWEMP.SoftwareManager.QA.UnitTesting/SoftwareManagerTest.cs
@@ -0,0 +1,16 @@
+namespace TWEMP.SoftwareManager.QA.UnitTesting
+{
+    public class SoftwareManagerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ExecuteStandardTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
